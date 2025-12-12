16a3fdf2b90fb3cea4b572fc8669577a40aad6d4
Sai Sailu Shaik <saisailushaik@Sailus-MacBook-Air.local>
2025-12-12 19:55:10 +0530

    #User added and role assigned

--- main.tf
+++ main.tf
@@ -29,3 +29,37 @@ resource "azurerm_resource_group" "rg1" {
 #   location            = azurerm_resource_group.example.location
 #   address_space       = ["10.0.0.0/16"]
 # }
+# Configure the Microsoft Entra ID Provider
+# It will automatically use the ARM_* environment variables from your TFC workspace
+provider "azuread" {} 
+
+# --- STEP 1: Create the User Account ---
+resource "azuread_user" "new_user" {
+  user_principal_name = var.new_user_upn
+  display_name        = var.new_user_display_name
+  mail_nickname       = split("@", var.new_user_upn)[0]
+  
+  # Set initial login credentials
+  password            = var.initial_password
+  force_password_change = true
+  
+  # Account must be enabled to be assigned a role
+  account_enabled     = true 
+}
+
+# --- STEP 2: Lookup the Directory Role ---
+data "azuread_directory_role" "target_role" {
+  display_name = var.directory_role_name
+}
+
+# --- STEP 3: Assign the Directory Role to the New User ---
+resource "azuread_directory_role_assignment" "user_role_assignment" {
+  # The ID of the role template
+  role_id             = data.azuread_directory_role.target_role.template_id
+
+  # The Object ID of the newly created user (the principal)
+  principal_object_id = azuread_user.new_user.object_id
+
+  # The scope is the entire Entra ID tenant
+  directory_scope_id  = azuread_user.new_user.tenant_id
+}
